b7107d910f6be1e65e25d6b83ec3a43706f5b5d9
Taeyun Ryu <159678393+tyroneryu@users.noreply.github.com>
2025-04-07 11:05:38 +0900

    Create CloseBtn.tsx
    refactor: tailwind style refactoring

---
+++ src/components/button/CloseBtn.tsx
@@ -0,0 +1,24 @@
+import tw from 'tailwind-styled-components';
+import IconClose from '../../assets/icons/icon_close.svg';
+
+export const CloseButton = tw.button`
+  w-[40px]
+  h-[40px]
+  flex
+  items-center
+  justify-center
+  bg-lightGray
+  rounded-full
+
+  hover:bg-[#525252]
+`;
+
+function CloseBtn({ toggleModal }: { toggleModal?: () => void }) {
+  return (
+    <CloseButton onClick={toggleModal}>
+      <img src={IconClose} alt="Close" />
+    </CloseButton>
+  )
+}
+
+export default CloseBtn
