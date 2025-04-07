1b038e4e75d74b143da764602efce93b6b1a4b3f
Taeyun Ryu <159678393+tyroneryu@users.noreply.github.com>
2025-04-07 11:31:19 +0900

    Create theme.ts
    chore: markup the first page

---
+++ src/styles/theme.ts
@@ -0,0 +1,19 @@
+import themeT from "./themeType";
+
+export const size = {
+  tablet: "640px",
+  laptop: "1200px",
+  desktop: "1800px",
+};
+
+export const theme: themeT = {
+  mainColorBlack: '#232323',
+  mainColorGray: '#EFEFF1',
+  darkGray: '#0C0C0C',
+  lightGray: '#373737',
+  superLightGray: '#F1F2F5',
+  white: '#fff',
+  black: '#000',
+};
+
+export default theme;
