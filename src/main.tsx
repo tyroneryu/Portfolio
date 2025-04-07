f94fea0d3bf3560c1fde904f080f682158f53d17
Taeyun Ryu <159678393+tyroneryu@users.noreply.github.com>
2025-04-07 11:43:08 +0900

    Create main.tsx
    chore: redux toolkit setting

---
+++ src/main.tsx
@@ -0,0 +1,17 @@
+import ReactDOM from 'react-dom/client'
+import { Provider } from 'react-redux';
+import { ThemeProvider } from 'styled-components';
+import GlobalStyle from './styles/GlobalStyles';
+import theme from './styles/theme';
+import './index.css'
+import App from './App';
+import store from './store'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <ThemeProvider theme={theme}>
+    <Provider store={store}>
+      <GlobalStyle />
+      <App />
+    </Provider>
+  </ThemeProvider>,
+)
