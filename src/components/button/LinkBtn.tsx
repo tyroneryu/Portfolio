b6345a3a9664b024f73c1d67b6b20ac7acd942bc
Taeyun Ryu <159678393+tyroneryu@users.noreply.github.com>
2025-04-07 11:09:12 +0900

    Create LinkBtn.tsx
    feat: add tooltip

---
+++ src/components/button/LinkBtn.tsx
@@ -0,0 +1,79 @@
+import tw from 'tailwind-styled-components';
+import HoverModal from '../modal/HoverModal';
+import { ICON_URL } from '../../utils/constant/constant';
+
+type BlogT = {
+  title?: string;
+  url?: string;
+}
+
+type LinkProps = {
+  name: string;
+  text: string;
+  giturl?: string | undefined;
+  depoloyurl?: string | undefined;
+  blog?: BlogT[];
+}
+
+export const LinkButton = tw.a`
+  relative
+  flex
+  flex-col
+  items-center
+  justify-center
+  gap-1
+  px-3
+  w-full
+  group
+
+  max-md:px-0
+`;
+
+export const IconCircle = tw.i`
+  w-[47px]
+  h-[47px]
+  flex
+  items-center
+  justify-center
+  bg-white
+  border-solid
+  rounded-full
+  border
+  border-[#dcdcdc]
+
+  max-md:w-[37px]
+  max-md:h-[37px]
+`;
+
+export const Img = tw.img`
+  max-md:w-[50%]
+`;
+
+export const Text = tw.span`
+  text-white
+  text-xs
+  font-medium
+
+  max-md:text-mainBlack
+  max-md:hidden
+`;
+
+function LinkBtn({ name, text, giturl, depoloyurl, blog }: LinkProps) {
+  return (
+    <>
+      {
+        depoloyurl === '' ? null : (
+          <LinkButton href={giturl ? giturl : depoloyurl} aria-label={text}>
+            <IconCircle>
+              <Img src={`${ICON_URL}${name}.svg`} alt={name} />
+            </IconCircle>
+            <Text>{text}</Text>
+            {blog && <HoverModal blog={blog} />}
+          </LinkButton>
+        )
+      }
+    </>
+  );
+}
+
+export default LinkBtn
