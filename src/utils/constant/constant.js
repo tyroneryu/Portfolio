e2644c0e19d45e2063430ea5ca0d03db0d7f28a3
Taeyun Ryu <159678393+tyroneryu@users.noreply.github.com>
2025-04-07 11:28:46 +0900

    Create constant.js
    docs

---
+++ src/utils/constant/constant.js
@@ -0,0 +1,46 @@
+export const ICON_URL =
+  'https://raw.githubusercontent.com/jieun419/jieun_portfolio/main/src/assets/icons/icon_';
+
+export const UPDATE_DATA = '24. 01. 06';
+export const EMAILL = 'crg1050@gmail.com';
+
+export const OPACITY_0 = { opacity: 0 };
+export const OPACITY_1 = { opacity: 1 };
+
+export const Y_0 = { y: 0 };
+export const Y_M10 = { y: -10 };
+export const Y_P10 = { y: 10 };
+export const Y_M20 = { y: -20 };
+export const Y_P20 = { y: 20 };
+
+export const ANI_Y_OFF = { opacity: 0, y: -10 };
+export const ANI_Y_ON = { opacity: 1, y: 0 };
+
+export const ANI_TRANSITION = { ease: 'easeOut', duration: 0.8 };
+
+export const socialLink = [
+  {
+    id: 1,
+    datatooltip: '블로그 가기',
+    tooltiptype: 'bottom',
+    href: 'https://velog.io/@crg1050',
+    arialabel: 'Velog',
+    target: '_blank',
+  },
+  {
+    id: 2,
+    datatooltip: 'GitHub 가기',
+    tooltiptype: 'bottom',
+    href: 'https://github.com/jieun419',
+    arialabel: 'GitHub',
+    target: '_blank',
+  },
+  {
+    id: 3,
+    datatooltip: '이력서 보기',
+    tooltiptype: 'bottom',
+    href: 'https://docs.google.com/document/d/1ahAvIVKQe9wBEwYYQG86Ks8-ScnuFFCQ2255TEVhSoo/edit?usp=sharing',
+    arialabel: 'Resume',
+    target: '_blank',
+  },
+];
