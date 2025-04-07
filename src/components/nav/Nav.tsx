520300ef3503914820da3c9e5415f2de78e87de5
Taeyun Ryu <159678393+tyroneryu@users.noreply.github.com>
2025-04-07 11:23:17 +0900

    Create Nav.tsx
    refactor

---
+++ src/components/nav/Nav.tsx
@@ -0,0 +1,49 @@
+import tw from 'tailwind-styled-components';
+import { TabsPropsT } from '../../types/type';
+
+export const NavContainer = tw.nav`
+  sticky
+  top-10
+  flex
+  flex-col
+  gap-3
+  w-auto
+  h-screen
+
+  max-md:flex-row
+  max-md:bg-mainBlack
+  max-md:z-[2]
+  max-md:py-3
+  max-md:top-0
+  max-md:w-full
+  max-md:h-auto
+  max-md:backdrop-blur-md
+`;
+
+export const NavLink = tw.button`
+  text-lightGray
+  font-black
+  text-6xl
+  transition
+
+  max-md:text-3xl
+  max-lg:text-5xl
+`;
+
+function Nav({ navTabs }: TabsPropsT) {
+  return (
+    <NavContainer>
+      {navTabs.map((tab, idx) => (
+        <NavLink
+          key={idx}
+          onClick={tab.scrollToTarget}
+          className={tab.isTarget ? 'text-mainGray' : 'text-lightGray hover:text-[#4a4a4a]'}
+        >
+          {tab.targetName}
+        </NavLink>
+      ))}
+    </NavContainer>
+  );
+}
+
+export default Nav;
