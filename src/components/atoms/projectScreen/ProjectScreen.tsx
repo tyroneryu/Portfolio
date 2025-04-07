78acb05e959872bf3c5aecab6669a8394491f251
Taeyun Ryu <159678393+tyroneryu@users.noreply.github.com>
2025-04-07 10:47:30 +0900

    Create ProjectScreen.tsx
    style: adaptive style

---
+++ src/components/atoms/projectScreen/ProjectScreen.tsx
@@ -0,0 +1,32 @@
+import tw from 'tailwind-styled-components';
+
+type ThumbnailT = {
+  imgurl: string;
+};
+
+export const ScreenContainer = tw.div`
+  bg-white  
+  border-solid
+  border-[#dcdcdc]
+  border-[0.0625rem]
+  p-3
+  w-[calc(100vw-60vw)]
+  rounded-[2.125rem]
+
+  max-md:w-[calc(100vw-10vw)]
+`;
+
+export const Img = tw.img`
+  w-full
+  rounded-[2.125rem]
+`;
+
+function ProjectScreen({ imgurl }: ThumbnailT) {
+  return (
+    <ScreenContainer>
+      <Img src={imgurl} alt='projectScreen' />
+    </ScreenContainer>
+  );
+}
+
+export default ProjectScreen;
